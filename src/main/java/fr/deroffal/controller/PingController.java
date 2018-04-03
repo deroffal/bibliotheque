eac065f1d122acf2019b730fd49e84d79fa2adbe
deroffal <alex_roffde@hotmail.com>
2018-04-03 21:27:45 +0200

    Documentation Swagger.

--- src/main/java/fr/deroffal/controller/PingController.java
+++ src/main/java/fr/deroffal/controller/PingController.java
@@ -1,17 +1,26 @@
 package fr.deroffal.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "/public", description = "Contrôleur public")
 @RestController
+@RequestMapping(value = "/public")
 public class PingController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PingController.class);
 
+    @ApiOperation(
+            httpMethod = "GET",
+            value = "Un ping du serveur"
+    )
     @GetMapping(value = "/ping")
     public ResponseEntity<String> ping() {
         LOGGER.info("Démarrage de l'appli OK !");
