d18f3ba5f24b912cd5b1fbe5c599b440965785c3
deroffal <alex_roffde@hotmail.com>
2018-04-10 17:38:29 +0200

    TU pour la couche service.

---
+++ documentation/test.md
@@ -0,0 +1,12 @@
+# Test
+## Test unitaire
+On utilise Junit5 comme framework de test unitaire.  
+### Couche de persistance
+### Couche de service
+Cas d'exemple : [UserServiceTest.java](../master/src/test/java/fr/portail/deroffal/user/service/UserServiceTest.java)
+
+On va utiliser Mockito pour bouchonner les injections des dépendances de nos services.
+Il n'existe pas encore de runner pour remplacer le `@RunWith` de Junit4. On va utiliser la classe [MockitoExtension.java](../master/src/test/java/fr/portail/deroffal/util/MockitoExtension.java)
+qui est reprise de [Junit](https://github.com/junit-team/junit5-samples/blob/master/junit5-mockito-extension/src/main/java/com/example/mockito/MockitoExtension.java) pour 
+initiliser automatiquement nos mocks à l'aide de l'annotation : `@ExtendWith(MockitoExtension.class)`.
+### Couche de contrôleur
---
+++ src/test/java/fr/deroffal/portail/authentification/service/UserServiceTest.java
@@ -0,0 +1,88 @@
+package fr.deroffal.portail.authentification.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import fr.deroffal.portail.authentification.dao.UserDao;
+import fr.deroffal.portail.authentification.dto.UserDto;
+import fr.deroffal.portail.authentification.entity.RoleEntity;
+import fr.deroffal.portail.authentification.entity.UserEntity;
+import fr.deroffal.portail.authentification.mapping.UserMapper;
+import fr.deroffal.portail.util.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+	@InjectMocks
+	private UserService userService;
+
+	@Mock
+	private UserDao userDao;
+
+	@Mock
+	private UserMapper userMapper;
+
+	@Test
+	@DisplayName("Charger un utilisateur : Pas de user trouvé, on lève une exception.")
+	void loadUserByUsername_leveUsernameNotFoundException_siPasDeResultat() {
+		final String login = "login";
+
+		when(userDao.findByLogin(login)).thenReturn(null);
+
+		assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername(login));
+	}
+
+	@Test
+	@DisplayName("Charger un utilisateur : On trouve un utilisateur, on retourne le UserDetails correspondant.")
+	void loadUserByUsername_retourneUserDetails_siResultat() {
+		final UserEntity user = new UserEntity();
+		user.setId(1L);
+		final String login = "login";
+		user.setLogin(login);
+		final String password = "zyuatf!:l;mljhfomwizuf#w<z32120vw:!;,efli:zuj";
+		user.setPassword(password);
+
+		final RoleEntity role = new RoleEntity();
+		role.setRole("SUPER_ADMIN");
+		user.setRoles(List.of(role));
+
+		when(userDao.findByLogin(login)).thenReturn(user);
+
+		final UserDetails actualUser = userService.loadUserByUsername(login);
+		assertEquals(login, actualUser.getUsername());
+		assertEquals(password, actualUser.getPassword());
+
+		final Collection<? extends GrantedAuthority> actualUserAuthorities = actualUser.getAuthorities();
+		assertEquals(1, actualUserAuthorities.size());
+		assertEquals("ROLE_SUPER_ADMIN", actualUserAuthorities.iterator().next().getAuthority());
+	}
+
+	@Test
+	@DisplayName("Création d'un utilisateur.")
+	void createUser(){
+		final UserDto userDtoToSave = new UserDto();
+		final UserEntity userEntityToSave = new UserEntity();
+
+		final UserEntity savedUserEntity = new UserEntity();
+		final UserDto expectedUser = new UserDto();
+
+		when(userMapper.toEntityAndEncorePassword(userDtoToSave)).thenReturn(userEntityToSave);
+		when(userDao.save(userEntityToSave)).thenReturn(savedUserEntity);
+		when(userMapper.toDto(savedUserEntity)).thenReturn(expectedUser);
+
+		assertEquals(expectedUser, userService.createUser(userDtoToSave));
+	}
+}
