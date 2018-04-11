c6dc0996a986d6148856ea775d02782299ee0b43
deroffal <alex_roffde@hotmail.com>
2018-04-11 18:23:03 +0200

    Sans l'annotation, on initialise les mocks à la main.

--- src/test/java/fr/deroffal/portail/authentification/service/UserServiceTest.java
+++ src/test/java/fr/deroffal/portail/authentification/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package fr.deroffal.portail.authentification.service;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
@@ -7,11 +8,13 @@ import static org.mockito.Mockito.when;
 import java.util.Collection;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,68 +24,73 @@ import fr.deroffal.portail.authentification.dto.UserDto;
 import fr.deroffal.portail.authentification.entity.RoleEntity;
 import fr.deroffal.portail.authentification.entity.UserEntity;
 import fr.deroffal.portail.authentification.mapping.UserMapper;
-import fr.deroffal.portail.util.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
-	@InjectMocks
-	private UserService userService;
+    @InjectMocks
+    private UserService userService;
 
-	@Mock
-	private UserDao userDao;
+    @Mock
+    private UserDao userDao;
 
-	@Mock
-	private UserMapper userMapper;
+    @Mock
+    private UserMapper userMapper;
 
-	@Test
-	@DisplayName("Charger un utilisateur : Pas de user trouvé, on lève une exception.")
-	void loadUserByUsername_leveUsernameNotFoundException_siPasDeResultat() {
-		final String login = "login";
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
-		when(userDao.findByLogin(login)).thenReturn(null);
+    @Test
+    @DisplayName("Charger un utilisateur : Pas de user trouvé, on lève une exception.")
+    void loadUserByUsername_leveUsernameNotFoundException_siPasDeResultat() {
+        final String login = "login";
 
-		assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername(login));
-	}
+        when(userDao.findByLogin(login)).thenReturn(null);
 
-	@Test
-	@DisplayName("Charger un utilisateur : On trouve un utilisateur, on retourne le UserDetails correspondant.")
-	void loadUserByUsername_retourneUserDetails_siResultat() {
-		final UserEntity user = new UserEntity();
-		user.setId(1L);
-		final String login = "login";
-		user.setLogin(login);
-		final String password = "zyuatf!:l;mljhfomwizuf#w<z32120vw:!;,efli:zuj";
-		user.setPassword(password);
+        assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername(login));
+    }
 
-		final RoleEntity role = new RoleEntity();
-		role.setRole("SUPER_ADMIN");
-		user.setRoles(List.of(role));
+    @Test
+    @DisplayName("Charger un utilisateur : On trouve un utilisateur, on retourne le UserDetails correspondant.")
+    void loadUserByUsername_retourneUserDetails_siResultat() {
+        final UserEntity user = new UserEntity();
+        user.setId(1L);
+        final String login = "login";
+        user.setLogin(login);
+        final String password = "zyuatf!:l;mljhfomwizuf#w<z32120vw:!;,efli:zuj";
+        user.setPassword(password);
 
-		when(userDao.findByLogin(login)).thenReturn(user);
+        final RoleEntity role = new RoleEntity();
+        role.setRole("SUPER_ADMIN");
+        user.setRoles(List.of(role));
 
-		final UserDetails actualUser = userService.loadUserByUsername(login);
-		assertEquals(login, actualUser.getUsername());
-		assertEquals(password, actualUser.getPassword());
+        when(userDao.findByLogin(login)).thenReturn(user);
 
-		final Collection<? extends GrantedAuthority> actualUserAuthorities = actualUser.getAuthorities();
-		assertEquals(1, actualUserAuthorities.size());
-		assertEquals("ROLE_SUPER_ADMIN", actualUserAuthorities.iterator().next().getAuthority());
-	}
+        final UserDetails actualUser = userService.loadUserByUsername(login);
+        assertEquals(login, actualUser.getUsername());
+        assertEquals(password, actualUser.getPassword());
 
-	@Test
-	@DisplayName("Création d'un utilisateur.")
-	void createUser(){
-		final UserDto userDtoToSave = new UserDto();
-		final UserEntity userEntityToSave = new UserEntity();
+        final Collection<? extends GrantedAuthority> actualUserAuthorities = actualUser.getAuthorities();
+        assertAll("Un seul rôle : SUPER_ADMIN",
+                () -> assertEquals(1, actualUserAuthorities.size()),
+                () -> assertEquals("ROLE_SUPER_ADMIN", actualUserAuthorities.iterator().next().getAuthority())
+        );
+    }
 
-		final UserEntity savedUserEntity = new UserEntity();
-		final UserDto expectedUser = new UserDto();
+    @Test
+    @DisplayName("Création d'un utilisateur.")
+    void createUser() {
+        final UserDto userDtoToSave = new UserDto();
+        final UserEntity userEntityToSave = new UserEntity();
 
-		when(userMapper.toEntityAndEncorePassword(userDtoToSave)).thenReturn(userEntityToSave);
-		when(userDao.save(userEntityToSave)).thenReturn(savedUserEntity);
-		when(userMapper.toDto(savedUserEntity)).thenReturn(expectedUser);
+        final UserEntity savedUserEntity = new UserEntity();
+        final UserDto expectedUser = new UserDto();
 
-		assertEquals(expectedUser, userService.createUser(userDtoToSave));
-	}
+        when(userMapper.toEntityAndEncorePassword(userDtoToSave)).thenReturn(userEntityToSave);
+        when(userDao.save(userEntityToSave)).thenReturn(savedUserEntity);
+        when(userMapper.toDto(savedUserEntity)).thenReturn(expectedUser);
+
+        assertEquals(expectedUser, userService.createUser(userDtoToSave));
+    }
 }
