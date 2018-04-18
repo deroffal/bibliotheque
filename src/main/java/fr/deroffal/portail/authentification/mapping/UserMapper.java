c9457821b5c42e1548e05b7e5fc13c812514a8ef
deroffal <alex_roffde@hotmail.com>
2018-04-18 14:06:57 +0200

    Ajout TU mapper.

--- src/main/java/fr/deroffal/portail/authentification/mapping/UserMapper.java
+++ src/main/java/fr/deroffal/portail/authentification/mapping/UserMapper.java
@@ -18,6 +18,15 @@ public abstract class UserMapper {
 
 	public abstract UserDto toDto(final UserEntity user);
 
+	/**
+	 * Création d'en Entity pour un utilisateur.
+	 * Pas besoin de mapper l'ID et les rôles qui vont être déterminés par la suite (insertion en bdd, ou dans un service). Le mot de passe sera encodé dans le mapper.
+	 *
+	 * @param user un utilisateur à créer.
+	 *
+	 * @return un {@link UserEntity} initialisé pour l'insertion en base.
+	 */
+	@Mapping(target = "id", ignore = true)
 	@Mapping(target = "password", ignore = true)
 	@Mapping(target = "roles", ignore = true)
 	public abstract UserEntity toEntityAndEncorePassword(final UserDto user);
---
+++ src/test/java/fr/deroffal/portail/authentification/mapping/UserMapperTest.java
@@ -0,0 +1,72 @@
+package fr.deroffal.portail.authentification.mapping;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import fr.deroffal.portail.authentification.dto.UserDto;
+import fr.deroffal.portail.authentification.entity.UserEntity;
+
+class UserMapperTest {
+
+	@InjectMocks
+	final UserMapper mapper = Mappers.getMapper(UserMapper.class);
+
+	@Mock
+	private PasswordEncoder passwordEncoder;
+
+	@Test
+	@DisplayName("Mapping Entity -> DTO.")
+	void toDto() {
+		final UserEntity entity = new UserEntity();
+		final Long id = 35L;
+		entity.setId(id);
+		final String login = "login";
+		entity.setLogin(login);
+		final String password = "azerty";
+		entity.setPassword(password);
+
+		final UserDto dto = mapper.toDto(entity);
+
+		assertEquals(id, dto.getId());
+		assertEquals(login, dto.getLogin());
+		assertEquals(password, dto.getPassword());
+	}
+
+	@Test
+	@DisplayName("Mapping DTO -> Entity.")
+	void toEntityAndEncorePassword() {
+		MockitoAnnotations.initMocks(this);
+
+		final UserDto dto = new UserDto();
+		final String login = "login";
+		dto.setLogin(login);
+		final String password = "azerty";
+		dto.setPassword(password);
+
+		final String encodedPassword = "wxcvbn";
+		when(passwordEncoder.encode(password)).thenReturn(encodedPassword);
+
+		final UserEntity entity = mapper.toEntityAndEncorePassword(dto);
+		assertNull(entity.getId());
+		assertNull(entity.getRoles());
+
+		assertEquals(login, entity.getLogin());
+		assertEquals(encodedPassword, entity.getPassword());
+	}
+
+	@Test
+	@DisplayName("Test des cas où l'entrée est null.")
+	void test_entree_null(){
+		assertNull(mapper.toDto(null));
+		assertNull(mapper.toEntityAndEncorePassword(null));
+	}
+}
