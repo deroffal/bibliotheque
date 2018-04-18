00e4f4590d102a9baa5e4f900bd515c20a5bbd99
deroffal <alex_roffde@hotmail.com>
2018-04-18 19:30:36 +0200

    cleanup

--- src/main/java/fr/deroffal/portail/controller/PingController.java
+++ src/main/java/fr/deroffal/portail/controller/PingController.java
@@ -1,12 +1,5 @@
 package fr.deroffal.portail.controller;
 
-import java.io.FileOutputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-import org.dbunit.database.DatabaseConnection;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.xml.FlatDtdDataSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -23,13 +16,13 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping(value = "/public")
 public class PingController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(PingController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PingController.class);
 
-	@ApiOperation(httpMethod = "GET", value = "Un ping du serveur")
-	@GetMapping(value = "/ping")
-	public ResponseEntity<String> ping() {
-		LOGGER.info("OK !");
-		return new ResponseEntity<>("Pong", HttpStatus.OK);
-	}
+    @ApiOperation(httpMethod = "GET", value = "Un ping du serveur")
+    @GetMapping(value = "/ping")
+    public ResponseEntity<String> ping() {
+        LOGGER.info("OK !");
+        return new ResponseEntity<>("Pong", HttpStatus.OK);
+    }
 
 }
