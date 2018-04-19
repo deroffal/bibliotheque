3a50e25923c8d6eae9b999b0d4f14dd62cba00a9
deroffal <alex_roffde@hotmail.com>
2018-04-19 11:42:14 +0200

    Abstraction test intégration.

--- src/test/java/fr/deroffal/portail/AbstractIntegrationTest.java
+++ src/test/java/fr/deroffal/portail/AbstractIntegrationTest.java
@@ -1,4 +1,4 @@
-package fr.deroffal.portail.authentification.controller;
+package fr.deroffal.portail;
 
 import org.dbunit.DBTestCase;
 import org.dbunit.PropertiesBasedJdbcDatabaseTester;
@@ -6,35 +6,28 @@ import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import fr.deroffal.portail.PortailConfigutation;
-import fr.deroffal.portail.authentification.entity.UserEntity;
 
 @ExtendWith(SpringExtension.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class UserControllerWithRestIT extends DBTestCase {
+public abstract class AbstractIntegrationTest extends DBTestCase {
 
 	private static final String URL = "http://localhost:";
 
 	@Autowired
-	private TestRestTemplate restTemplate;
+	protected TestRestTemplate restTemplate;
 
 	@LocalServerPort
-	private int port;
+	protected int port;
 
-	UserControllerWithRestIT() {
+	public AbstractIntegrationTest() {
 		super();
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.h2.Driver");
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:h2:mem:portailtest");
@@ -56,25 +49,12 @@ class UserControllerWithRestIT extends DBTestCase {
 	@Override
 	protected IDataSet getDataSet() throws Exception {
 		final FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
-		return builder.build(this.getClass().getResourceAsStream("/dataset/authentification.xml"));
+		return builder.build(this.getClass().getResourceAsStream("/dataset/" + getDataSetName()));
 	}
 
-	@Test
-	@DisplayName("Integration avec appel REST")
-	void avec_appel_rest() {
-		ResponseEntity<UserEntity> response = restTemplate.getForEntity(buildUrl("/user/user2"), UserEntity.class);
-		assertNotNull(response);
+	protected abstract String getDataSetName();
 
-		assertEquals(HttpStatus.OK, response.getStatusCode());
-
-		final UserEntity actualUser = response.getBody();
-
-		assertEquals(3L, actualUser.getId().longValue());
-		assertEquals("user2", actualUser.getLogin());
-		assertEquals("$2a$10$IvID3zGmRTLpIB/uCnjxleEmk0hUe6Gyr9oKX6UqAZkWrb6xvrmvC", actualUser.getPassword());
-	}
-
-	private String buildUrl(String uri) {
+	protected String buildUrl(String uri) {
 		return URL + port + uri;
 	}
 
--- src/test/java/fr/deroffal/portail/authentification/controller/UserControllerIT.java
+++ src/test/java/fr/deroffal/portail/authentification/controller/UserControllerIT.java
@@ -1,69 +1,20 @@
 package fr.deroffal.portail.authentification.controller;
 
-import java.util.Collection;
-
-import org.dbunit.DBTestCase;
-import org.dbunit.PropertiesBasedJdbcDatabaseTester;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
 
-import fr.deroffal.portail.PortailConfigutation;
-import fr.deroffal.portail.authentification.entity.RoleEntity;
+import fr.deroffal.portail.AbstractIntegrationTest;
 import fr.deroffal.portail.authentification.entity.UserEntity;
 
-@ExtendWith(SpringExtension.class)
-@TestPropertySource(locations = "classpath:application-test.properties")
-@SpringBootTest
-class UserControllerIT extends DBTestCase {
+class UserControllerIT extends AbstractIntegrationTest {
 
-	@Autowired
-	private UserController userController;
-
-	UserControllerIT() {
-		super();
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.h2.Driver");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:h2:mem:portailtest");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "sa");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA, PortailConfigutation.SCHEMA_AUTHENTIFICATION);
-	}
-
-	@BeforeEach
-	void beforeEach() throws Exception {
-		super.setUp();
-	}
-
-	@AfterEach
-	void afterEach() throws Exception {
-		super.tearDown();
-	}
-
-	@Override
-	protected IDataSet getDataSet() throws Exception {
-		final FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
-		return builder.build(this.getClass().getResourceAsStream("/dataset/authentification.xml"));
-	}
-
-	/**
-	 * TODO : 	@Transactional pour remplacer des entityGraph...
-	 */
 	@Test
-	@Transactional
-	@DisplayName("Integration Controller -> BDD dbunit")
-	void tout_d_un_coup() {
-		final ResponseEntity<UserEntity> response = userController.getUserByLogin("user2");
+	@DisplayName("Integration avec appel REST")
+	void avec_appel_rest() {
+		ResponseEntity<UserEntity> response = restTemplate.getForEntity(buildUrl("/user/user2"), UserEntity.class);
+		assertNotNull(response);
 
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 
@@ -73,11 +24,11 @@ class UserControllerIT extends DBTestCase {
 		assertEquals("user2", actualUser.getLogin());
 		assertEquals("$2a$10$IvID3zGmRTLpIB/uCnjxleEmk0hUe6Gyr9oKX6UqAZkWrb6xvrmvC", actualUser.getPassword());
 
-		final Collection<RoleEntity> actualUserRoles = actualUser.getRoles();
-		assertEquals(1, actualUserRoles.size());
-		assertEquals("USER", actualUserRoles.iterator().next().getRole());
-
+		//TODO : vérifier les rôles (besoin d'entitygraph sur l'entity)
 	}
 
+	@Override
+	protected String getDataSetName() {
+		return "authentification.xml";
+	}
 }
-
