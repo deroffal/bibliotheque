a43103ab53790f1567a96b769534d7b7a6d8a65c
deroffal <32491733+deroffal@users.noreply.github.com>
2020-03-25 17:48:35 +0100

    Amélioration gestion dates

--- commons/src/main/java/fr/deroffal/bibliotheque/api/util/DateUtils.java
+++ commons/src/main/java/fr/deroffal/bibliotheque/api/util/DateUtils.java
@@ -3,30 +3,42 @@ package fr.deroffal.bibliotheque.api.util;
 import org.springframework.stereotype.Component;
 
 import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 @Component
 public class DateUtils {
 
-	private static final ZoneId DEFAULT_ZONE_ID = ZoneId.of("CET");
+	/**Default zone : "Europe/Paris".*/
+    private static final ZoneId DEFAULT_ZONE_ID = ZoneId.of("CET");
 
-	private Clock clock = Clock.system(DEFAULT_ZONE_ID);
+    private Clock clock = Clock.system(DEFAULT_ZONE_ID);
 
-	public LocalDateTime newLocalDateTime() {
-		return LocalDateTime.now(clock);
-	}
+    /**Wrapper for the current Instant (now).*/
+    public Instant now() {
+        return Instant.now(clock);
+    }
 
-	/*****************************************************************************************/
+    /**The LocalDateTime corresponding to the current Instant on the default timezone.*/
+    public LocalDateTime newLocalDateTime() {
+        return now().atZone(DEFAULT_ZONE_ID).toLocalDateTime();
+    }
 
-	/** A n'utiliser que dans les tests pour fixer date et heure. */
-	public void fixTime(LocalDateTime date) {
-		clock = Clock.fixed(date.atZone(DEFAULT_ZONE_ID).toInstant(), DEFAULT_ZONE_ID);
-	}
+    /*****************************************************************************************/
 
-	/** A n'utiliser que dans les tests pour remise à 0 de date et heure. */
-	public void unfixTime() {
-		clock = Clock.system(DEFAULT_ZONE_ID);
-	}
+    /**
+     * A n'utiliser que dans les tests pour fixer date et heure.
+     */
+    public void fixTime(LocalDateTime date) {
+        clock = Clock.fixed(date.atZone(DEFAULT_ZONE_ID).toInstant(), DEFAULT_ZONE_ID);
+    }
+
+    /**
+     * A n'utiliser que dans les tests pour remise à 0 de date et heure.
+     */
+    public void unfixTime() {
+        clock = Clock.system(DEFAULT_ZONE_ID);
+    }
 
 }
