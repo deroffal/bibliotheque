1325dd5fff65df537d1e6a749534133dd7609265
deroffal <alexandre.deroff@protonmail.com>
2022-01-03 17:51:29 +0100

    Utiliser une unique base de données en dev

--- authentification/src/main/resources/db/changelog/v2/01-tables.sql
+++ authentification/src/main/resources/db/changelog/v2/01-tables.sql
@@ -20,13 +20,3 @@ create table utilisateur
             unique,
     encoded_password varchar(255) not null
 );
-
-create table utilisateur_role
-(
-    user_id bigint not null
-        constraint fkg3pnkokyynkwptthli4kix8xs
-            references utilisateur,
-    role_id bigint not null
-        constraint fkd5v2bdog3wl0fxnlhnucxoxjl
-            references role_applicatif
-);
